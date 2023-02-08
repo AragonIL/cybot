92db5270798dc3cbc1c70ea3203374c4c0dc2af8
AragonIL <124801251+AragonIL@users.noreply.github.com>
2023-02-08 17:40:19 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,7 +11,11 @@ input.onGesture(Gesture.LogoDown, function () {
     פקמן.change(LedSpriteProperty.Y, -1)
 })
 let פקמן: game.LedSprite = null
+let גלידה = game.createSprite(randint(0, 4), randint(0, 4))
 פקמן = game.createSprite(2, 2)
 basic.forever(function () {
-	
+    if (פקמן.isTouching(גלידה)) {
+        גלידה.delete()
+        גלידה = game.createSprite(randint(0, 4), randint(0, 4))
+    }
 })
